e5d3e3edda796b8e39bfb3425f345ab4589b95c8
Bastien Saro Chassetuillier <bastien.saro.c@gmail.com>
2017-09-22 14:07:44 +0200

    False login + express server

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,62 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        lemj
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+  <div class="container">
+    <h1>Please login to see the secret content</h1>
+    <form v-if="!$store.state.authUser" @submit.prevent="login">
+      <p class="error" v-if="formError">{{ formError }}</p>
+      <p><i>To login, use <b>demo</b> as username and <b>demo</b> as password.</i></p>
+      <p>Username: <input type="text" v-model="formUsername" name="username" /></p>
+      <p>Password: <input type="password" v-model="formPassword" name="password" /></p>
+      <button type="submit">Login</button>
+    </form>
+    <div v-else>
+      Hello {{ $store.state.authUser.username }}!
+      <pre>I am the secret content, I am shown only when the use is connected.</pre>
+      <p><i>You can also refresh this page, you'll still be connected!</i></p>
+      <button @click="logout">Logout</button>
     </div>
-  </section>
+    <p><nuxt-link to="/secret">Super secret page</nuxt-link></p>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+  export default {
+    data () {
+      return {
+        formError: null,
+        formUsername: '',
+        formPassword: ''
+      }
+    },
+    methods: {
+      async login () {
+        try {
+          await this.$store.dispatch('login', {
+            username: this.formUsername,
+            password: this.formPassword
+          })
+          this.formUsername = ''
+          this.formPassword = ''
+          this.formError = null
+        } catch (e) {
+          this.formError = e.message
+        }
+      },
+      async logout () {
+        try {
+          await this.$store.dispatch('logout')
+        } catch (e) {
+          this.formError = e.message
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .container {
+    padding: 100px;
+  }
+  .error {
+    color: red;
+  }
 </style>
